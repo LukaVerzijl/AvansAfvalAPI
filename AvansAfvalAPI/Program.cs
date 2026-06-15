760a5c31b18de7322b576bf0352caaac5e2874ce
dMATTb <mra060620@gmail.com>
2026-06-15 12:52:36 +0200

    Update Program.cs

--- AvansAfvalAPI/Program.cs
+++ AvansAfvalAPI/Program.cs
@@ -79,6 +79,6 @@ app.UseHttpsRedirection();
 app.UseAuthentication();
 app.UseAuthorization();
 app.MapGroup("/account").MapIdentityApi<IdentityUser>().WithTags("Account");
-
+app.MapControllers();
 
 app.Run();
