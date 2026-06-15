0b5049454fa700e022709d643c025e17e78e6678
dMATTb <mra060620@gmail.com>
2026-06-15 08:50:21 +0200

    Update db context, Create TrashController

---
+++ AvansAfvalAPI/Controllers/TrashController.cs
@@ -0,0 +1,33 @@
+using AvansAfvalAPI.Database;
+using AvansAfvalAPI.Interfaces;
+using AvansAfvalAPI.models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace AvansAfvalAPI.Controllers;
+
+
+[Authorize]
+[ApiController]
+[Route("[controller]")]
+[Consumes("application/json")]
+[Produces("application/json")]
+public class TrashController : ControllerBase
+{
+    private readonly DatabaseContext _context;
+    private readonly IAuthenticationService _authenticationService;
+    
+    public TrashController(DatabaseContext context, IAuthenticationService authenticationService)
+    {
+        _context = context;
+        _authenticationService = authenticationService;
+    }
+
+    [HttpGet(Name = "GetAllTrash")]
+    public async Task<ActionResult<TrashModel>> GetAsync()
+    {
+        var trash = await _context.Trash.ToListAsync();
+        return Ok(trash);
+    }
+}
--- AvansAfvalAPI/Database/DatabaseContext.cs
+++ AvansAfvalAPI/Database/DatabaseContext.cs
@@ -1,3 +1,4 @@
+using AvansAfvalAPI.models;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
@@ -6,11 +7,15 @@ namespace AvansAfvalAPI.Database;
 
 public class DatabaseContext(DbContextOptions<DatabaseContext> options) : IdentityDbContext<IdentityUser>(options)
 {
+    public DbSet<TrashModel> Trash { get; set; }
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
         base.OnModelCreating(modelBuilder);
+        
         modelBuilder.Entity<IdentityUser>()
             .HasIndex(u => u.NormalizedEmail)
             .IsUnique();
+
+        modelBuilder.Entity<TrashModel>();
     }
 }
